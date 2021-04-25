40aa3ce436278646b6ba1ae3420332a479bc1591
fishbot <mbmcmullen27@gmail.com>
2021-04-25 15:05:53 -0500

    read config from file and pass it as global

---
+++ README.md
@@ -0,0 +1,6 @@
+# go-wasm
+
+- wasm_exec.js 
+```sh
+cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" ./
+```
--- boot.js
+++ boot.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 require('./wasm_exec.js');
 
+global.kubeconfig = fs.readFileSync('/home/fishbot/.kube/config');
   
 (async () => {
     const go = new Go();
-    // global().set("kubeconfig", js.String(fs.readFileSync('/home/fishbot/.kube/config')))
 
     const mod = await WebAssembly.compile(fs.readFileSync('./main.wasm')) 
     let inst = await WebAssembly.instantiate(mod, go.importObject)
     await go.run(inst)
+    // getPods()
     //inst = await WebAssembly.instantiate(mod, go.importObject); //I guess it consumes the whole instance?
 })()
